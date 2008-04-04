d6a10e94b429505e1226db30774bd1caf66f90bd
Michael Trier <mtrier@gmail.com>
2008-04-04 00:29:18 -0400

    Had to revert back flush changes because they're causing all tests to fail.  Need to look into it more.  It has to do with committing the session.  Refactored out management.sql code so it's duplicated.

--- django_sqlalchemy/management/sql.py
+++ django_sqlalchemy/management/sql.py
@@ -1,19 +1,18 @@
 from django.db.models.loading import get_models
 from sqlalchemy import create_engine
-from django_sqlalchemy.backend import metadata
+from django_sqlalchemy.backend import metadata, Session
 
 def reset(engine, app):
-    tables = []
-    for model in get_models(app):
-        tables.append(model.__table__)
-        tables.extend([f.__table__ for f in model._meta.local_many_to_many])
-    metadata.drop_all(engine, tables=tables)
-
+    metadata.drop_all(engine, tables=_get_tables_for_app(app))
+    Session.commit()
 
 def create(engine, app):
+    metadata.create_all(engine, tables=_get_tables_for_app(app))
+    Session.commit()
+
+def _get_tables_for_app(app):
     tables = []
     for model in get_models(app):
         tables.append(model.__table__)
         tables.extend([f.__table__ for f in model._meta.local_many_to_many])
-    metadata.create_all(engine, tables=tables)
-
+    return tables
